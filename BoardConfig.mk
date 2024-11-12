c6affabca1a8ad66d7cfc72ef0267ecd82c2099f
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-11-12 05:55:59 -0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,9 +30,6 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
 
-TARGET_GLOBAL_CFLAGS   += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-
 # Assert
 TARGET_OTA_ASSERT_DEVICE := grandpplte
 
